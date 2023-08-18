08cfc86aed23f1d5eeb13f0266f6ad971b1795dd
PRANAV <pranavprasanth907@gmail.com>
2023-08-18 12:49:16 +0530

    baseUrl

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,3 +1,2 @@
-// export const baseUrl = 'https://dataflexserver-production.up.railway.app/api'
-export const baseUrl = 'http://localhost:3000/api'
+export const baseUrl = 'https://dataflexserver-production.up.railway.app/api'
 export const bearer = 'dflex'
